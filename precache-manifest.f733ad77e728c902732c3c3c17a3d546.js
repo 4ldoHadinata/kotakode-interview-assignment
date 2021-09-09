655b405b97cbddd04d92e4bd4c1ebd4aa7492337
4ldoHadinata <4ldohadinata@gmail.com>
2021-09-09 21:39:40 +0700

    Updates

---
+++ precache-manifest.f733ad77e728c902732c3c3c17a3d546.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9b3c4666cb8182cded1f6e700859322",
+    "url": "/kotakode-interview-assignment/index.html"
+  },
+  {
+    "revision": "6f8d88f3508004489415",
+    "url": "/kotakode-interview-assignment/static/css/main.a3accfed.chunk.css"
+  },
+  {
+    "revision": "d143df103890d7ed09a9",
+    "url": "/kotakode-interview-assignment/static/js/2.744fca30.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/kotakode-interview-assignment/static/js/2.744fca30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f8d88f3508004489415",
+    "url": "/kotakode-interview-assignment/static/js/main.dca52908.chunk.js"
+  },
+  {
+    "revision": "aa3d09a9dbf1c9ba7052",
+    "url": "/kotakode-interview-assignment/static/js/runtime-main.c597d623.js"
+  }
+]);
